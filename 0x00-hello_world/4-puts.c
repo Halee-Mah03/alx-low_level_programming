8eb58e061b1afc5029d53339950009469ee6fedb
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-16 20:14:11 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char str1[15];
+
+   strcpy(str, "Programming is like building a multilingual puzzle");
+   
+
+   puts(str);
+   
+   return(0);
+}
+
