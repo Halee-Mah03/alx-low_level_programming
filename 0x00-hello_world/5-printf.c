f3a2edf52d481a17b8892dd0c2d39274099d92aa
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-18 03:37:26 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, /n");
+	printf("with proper grammar, but the outcome is a piece of art, %c");
 		return (0);
 }
 
