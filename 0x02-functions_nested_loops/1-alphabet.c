51ce2271620994ffb9295d68915974e0f79782b5
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-21 09:51:40 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,13 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void)
+int main(void)
 {
     char ch; 
 
     for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-
-	putchar('\n');
-    }
+	    putchar(ch);
+    
+    putchar('\n');
+    
 }
