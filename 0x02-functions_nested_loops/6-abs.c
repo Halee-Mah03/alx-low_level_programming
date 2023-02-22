72124c98c73640e880859e6754182a34669a775e
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-22 14:16:56 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - to compute absolute integer
+ *
+ * @n: the integer to be checked
+ *
+ * Return: the absolute integer
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+
+		return (n);
+	else
+		return (-n);
+
+}
