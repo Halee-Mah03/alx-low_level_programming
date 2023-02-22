5b4e413df3b26fdf36ca6152102d5d281fc115d5
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-22 13:50:02 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,9 @@
  *
  * @n: the number to print the sign
  *
- *Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero,
+ * 	0 and prints 0 if n is zero,
+ *	-1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
