ab89fed753e3b0d05e3c59e44a0b500791c65dfa
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-28 11:37:38 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -25,8 +25,6 @@ void puts_half(char *str);
 
 void print_array(int *a, int n);
 
-char *_strcpy(char *dest, char *src)
-
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src);
 
 #endif
