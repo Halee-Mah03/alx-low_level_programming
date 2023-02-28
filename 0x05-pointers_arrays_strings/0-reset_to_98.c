968fb8eba6fde2955c8e03717e0905922fc384c7
Halee-Mah03@gmail.com <ojuolape05@gmail.com>
2023-02-28 02:41:37 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and update the pointed value to 98
+ *
+ * @n: the integer to be executed
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+
+}
